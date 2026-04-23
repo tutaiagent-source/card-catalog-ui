fe582a6d4a05657f44b6196bcb71923c0b2e97de
Rip <rip@Rips-iMac.local>
2026-04-22 20:23:03 -0700

    Disable caching for listed share pages

--- app/listed/share/[token]/page.tsx
+++ app/listed/share/[token]/page.tsx
@@ -1,6 +1,9 @@
 import { supabaseAdmin } from "@/lib/supabaseAdminClient";
 import ListingsSharedView from "@/components/ListingsSharedView";
 
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 export default async function ListedSharePage({
   params,
 }: {
